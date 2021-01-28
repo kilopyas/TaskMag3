1239cc4d5e86c5fed6c91edab82d501fb0ff26e1
kilopyas <56266668+kilopyas@users.noreply.github.com>
2021-01-28 18:24:16 +0800

    Partial

---
+++ TaskVendor3/TaskModule3/Controller/Cart/CouponPost.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace TaskVendor3\TaskModule3\Controller\Cart;
+
+class CouponPost extends \Magento\Checkout\Controller\Cart\CouponPost
+{
+    protected $quoteRepository;
+    protected $couponFactory;
+
+    public function execute()
+    {
+        $couponCode = $this->getRequest()->getParam('remove') == 1
+            ? ''
+            : trim($this->getRequest()->getParam('coupon_code'));
+
+        $cartQuote = $this->cart->getQuote();
+        $oldCouponCode = $cartQuote->getCouponCode();
+        $codeLength = strlen($couponCode);
+        if (!$codeLength && !strlen($oldCouponCode)) {
+            return $this->_goBack();
+        }
+        try {
+            $isCodeLengthValid = $codeLength && $codeLength <= \Magento\Checkout\Helper\Cart::COUPON_CODE_MAX_LENGTH;
+
+            $itemsCount = $cartQuote->getItemsCount();
+            if ($itemsCount) {
+                $cartQuote->getShippingAddress()->setCollectShippingRates(true);
+                $cartQuote->setCouponCode($isCodeLengthValid ? $couponCode : '')->collectTotals();
+                $this->quoteRepository->save($cartQuote);
+            }
+            
+            if ($codeLength) {
+                $escaper = $this->_objectManager->get(\Magento\Framework\Escaper::class);
+                $coupon = $this->couponFactory->create();
+                $coupon->load($couponCode, 'code');
+                if (!$itemsCount) {
+                    if ($isCodeLengthValid && $coupon->getId()) {
+                        $this->_checkoutSession->getQuote()->setCouponCode($couponCode)->save();
+                        $this->messageManager->addSuccessMessage(
+                            __(
+                                'You used coupon code "%1".',
+                                $escaper->escapeHtml($couponCode)
+                            )
+                        );
+                    } else {
+                        $this->messageManager->addErrorMessage(
+                            __(
+                                'The coupon code "%1" is not valid.',
+                                $escaper->escapeHtml($couponCode)
+                            )
+                        );
+                    }
+                } else {
+                    if ($isCodeLengthValid && $coupon->getId() && $couponCode == $cartQuote->getCouponCode()) {
+                        $this->messageManager->addSuccessMessage(
+                            __(
+                                'You used coupon code "%1".',
+                                $escaper->escapeHtml($couponCode)
+                            )
+                        );
+                    } else {
+                        $this->messageManager->addErrorMessage(
+                            __(
+                                'The coupon code "%1" is not valid.',
+                                $escaper->escapeHtml($couponCode)
+                            )
+                        );
+                    }
+                }
+            } else {
+                $this->messageManager->addSuccessMessage(__('You canceled the coupon code.'));
+            }
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage(__('We cannot apply the coupon code.'));
+            $this->_objectManager->get(\Psr\Log\LoggerInterface::class)->critical($e);
+        }
+
+        return $this->_goBack();
+    }
+}
---
+++ TaskVendor3/TaskModule3/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'TaskVendor3_TaskModule3',
+	__DIR__
+);
